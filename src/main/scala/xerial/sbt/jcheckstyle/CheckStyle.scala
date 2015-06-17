3e3066f7ddc1c767444928e2511b6260738a32dc
Taro L. Saito <leo@xerial.org>
2015-06-18 00:52:03 +0900

    Call checker

--- src/main/scala/xerial/sbt/jcheckstyle/CheckStyle.scala
+++ src/main/scala/xerial/sbt/jcheckstyle/CheckStyle.scala
@@ -1,8 +1,10 @@
 package xerial.sbt.jcheckstyle
 
+import com.puppycrawl.tools.checkstyle.{PropertiesExpander, ConfigurationLoader, Checker}
 import sbt._
 import sbt.Keys._
 import sbt.plugins.JvmPlugin
+import scala.collection.JavaConversions._
 
 /**
  *
@@ -12,7 +14,6 @@ class CheckStyle extends AutoPlugin
   trait CheckStyleKeys {
     val checkStyleConfig = settingKey[File]("Path to checkstyle-config.xml file")
     val checkStyle = taskKey[Boolean]("Run checkstyle")
-
   }
 
   object CheckStyleKeys extends CheckStyleKeys {
@@ -30,6 +31,11 @@ class CheckStyle extends AutoPlugin
   lazy val checkStyleSettings = Seq[Setting[_]](
     checkStyleConfig := baseDirectory.value / "checkstyle-config.xml",
     checkStyle := {
+
+      val loader = ConfigurationLoader.loadConfiguration(checkStyleConfig.value.getPath, new PropertiesExpander(System.getProperties))
+      val checker = new Checker()
+      checker.configure(loader)
+      checker.process(Seq.empty)
 
       true
     }
